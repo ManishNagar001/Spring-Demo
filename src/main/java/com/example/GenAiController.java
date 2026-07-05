d173bd50d273562d74ab388348a56e430303aebc
Manish Nagar <mnagargujjar33@gmail.com>
2026-07-05 22:05:30 +0530

    Your commit message

--- src/main/java/com/example/GenAiController.java
+++ src/main/java/com/example/GenAiController.java
@@ -12,62 +12,62 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 public class GenAiController {
-	
+
 	ChatService chatService;
-	//ImageService imageService;
+	// ImageService imageService;
 	RecipeGenerator recipeGeneratior;
-	
-	
-	public GenAiController(ChatService chatService, 
-			//ImageService imageService,
+
+	public GenAiController(ChatService chatService,
+			// ImageService imageService,
 			RecipeGenerator recipeGeneratior) {
 		this.chatService = chatService;
-		//this.imageService=imageService;
-		this.recipeGeneratior=recipeGeneratior;
+		// this.imageService=imageService;
+		this.recipeGeneratior = recipeGeneratior;
 	}
 
 	@GetMapping("/test")
 	public String test() {
-		return "Test is succesffuly done, server is running";                             
+		return "Test is succesffuly done, server is running, 2nd Test case";
 	}
 
 	@GetMapping("/askai")
 	public String getResponse(@RequestParam String prompt) {
-		return chatService .getResponse(prompt);
+		return chatService.getResponse(prompt);
 	}
-	
-//	@GetMapping("/generateImage")
-//	public void generateImage(HttpServletResponse response,@RequestParam String prompt) throws IOException {
-//		ImageResponse imageResponse= imageService.imageGeneration(prompt);
-//		String url=imageResponse.getResult().getOutput().getUrl();
-//		response.sendRedirect(url);
-//	}
-//	
-//	@GetMapping("/generateImages")
-//	public List<String> generateImages(HttpServletResponse response,@RequestParam String prompt,
-//            @RequestParam(defaultValue="hd") String quality,
-//            @RequestParam(defaultValue="1") int n,
-//            @RequestParam(defaultValue="1024") int height,
-//            @RequestParam(defaultValue="1024") int width
-//            
-//			) throws IOException {
-//		ImageResponse imageResponse= imageService.imageGeneration(prompt,quality,n,height,width);
-//		
-//		List<String> urls=imageResponse.getResults().stream()
-//				.map(result->result.getOutput().getUrl()).toList();
-//		return urls;
-//	}
-	
+
+	// @GetMapping("/generateImage")
+	// public void generateImage(HttpServletResponse response,@RequestParam String
+	// prompt) throws IOException {
+	// ImageResponse imageResponse= imageService.imageGeneration(prompt);
+	// String url=imageResponse.getResult().getOutput().getUrl();
+	// response.sendRedirect(url);
+	// }
+	//
+	// @GetMapping("/generateImages")
+	// public List<String> generateImages(HttpServletResponse response,@RequestParam
+	// String prompt,
+	// @RequestParam(defaultValue="hd") String quality,
+	// @RequestParam(defaultValue="1") int n,
+	// @RequestParam(defaultValue="1024") int height,
+	// @RequestParam(defaultValue="1024") int width
+	//
+	// ) throws IOException {
+	// ImageResponse imageResponse=
+	// imageService.imageGeneration(prompt,quality,n,height,width);
+	//
+	// List<String> urls=imageResponse.getResults().stream()
+	// .map(result->result.getOutput().getUrl()).toList();
+	// return urls;
+	// }
+
 	@GetMapping("/recipeGenerator")
 	public String recipeGenerator(@RequestParam String ingredients,
-			@RequestParam(defaultValue="any") String cusine,
-			@RequestParam(defaultValue="") String dietartRestrication
-			)  {
+			@RequestParam(defaultValue = "any") String cusine,
+			@RequestParam(defaultValue = "") String dietartRestrication) {
 		try {
-		return recipeGeneratior.createRecipe(ingredients, cusine, dietartRestrication);
+			return recipeGeneratior.createRecipe(ingredients, cusine, dietartRestrication);
+		} catch (Exception e) {
+			return "EROOR " + e.getMessage();
 		}
-		catch(Exception e) {
-			return "EROOR "+ e.getMessage();
-		}
-}
+	}
 }
