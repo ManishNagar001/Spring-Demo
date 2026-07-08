7690ead35614d4ee58d638b63a745fd27a0c2cf9
ManishNagar001 <165934333+ManishNagar001@users.noreply.github.com>
2026-07-08 22:02:20 +0530

    Log GOOGLE_GENAI_API_KEY on application start

--- src/main/java/com/example/AiApplication.java
+++ src/main/java/com/example/AiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AiApplication {
 
 	public static void main(String[] args) {
+		  System.out.println("GOOGLE_GENAI_API_KEY = " + System.getenv("GOOGLE_GENAI_API_KEY"));
 		SpringApplication.run(AiApplication.class, args);
 	}
 
